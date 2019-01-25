c68083232ff3143f6bbbbf3bfb3ae370e8d6e851
Achala Dias <achala.d@platformer.com>
2019-01-26 03:11:12 +0530

    alpha-1.0.0

--- run.py
+++ run.py
@@ -2,21 +2,27 @@ from flask_api import FlaskAPI
 from flask_restful import Api
 from flask_cors import CORS
 import os
+from redis import Redis
+from flask import Flask, request, jsonify
 
-api_version = '/v1'
 
 app = FlaskAPI(__name__)
+redis = Redis(host="redis", db=0, socket_timeout=5, charset="utf-8", decode_responses=True)
 CORS(app)
-Rapi = Api(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return {'version': 'Pomidor - version 1.2.3'}, 200
+    return {'version': 'version 1.0.0'}, 200
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
 
-app.debug = True
-host = os.environ.get('IP', '0.0.0.0')
-port = int(os.environ.get('PORT', 5000))
-app.run(host=host, port=port)
+    if request.method == 'POST':
+        name = request.json['name']
+        redis.rpush('students', {'name': name})
+        return jsonify({'name': name})
+
+    if request.method == 'GET':
+        return jsonify(redis.lrange('students', 0, -1))
 
